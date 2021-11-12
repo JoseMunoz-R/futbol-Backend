c5f367ce43c2750723d75af5dc8a9de93beceee7
Jose Muñoz <jjose.munoz64@gmail.com>
2021-11-12 08:26:40 -0500

    AutenticaciónUsuarios

--- src/main/java/com/backend/futbol/controllers/UsuarioController.java
+++ src/main/java/com/backend/futbol/controllers/UsuarioController.java
@@ -1,10 +1,12 @@
 package com.backend.futbol.controllers;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.backend.futbol.models.UsuarioModel;
 import com.backend.futbol.services.UsuarioService;
+import com.backend.futbol.utils.Autorizacion;
 import com.backend.futbol.utils.BCrypt;
 import com.backend.futbol.exceptions.CustomExpeption;
 
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
 @RequestMapping("/api")
@@ -43,6 +48,32 @@ public class UsuarioController {
         }
         return ResponseEntity.ok(respuesta);
         
+    }
+
+    @PostMapping("/usuarios/login")
+    public ResponseEntity<UsuarioModel> login(@RequestBody UsuarioModel usuario){
+        UsuarioModel u= usuarioService.buscarPorUsername(usuario.getUsername());
+        if(u.getNombre()== null){
+            throw new CustomExpeption("Usuario o contraseña incorrectos");
+        }
+        if(!BCrypt.checkpw(usuario.getPassword(), u.getPassword())){
+            throw new CustomExpeption("Usuario o contraseña incorrectos");
+        }
+        String hash="";
+        long tiempo= System.currentTimeMillis();
+
+        if(u.getId()!=""){
+            hash=Jwts.builder()
+            .signWith(SignatureAlgorithm.HS256, Autorizacion.KEY)
+            .setSubject(u.getNombre())
+            .setIssuedAt(new Date (tiempo))
+            .setExpiration(new Date (tiempo+9000000))
+            .claim("username", u.getUsername())
+            .claim("correo", u.getCorreo())
+            .compact();
+        }
+        u.setHash(hash);
+        return ResponseEntity.ok(u);
     }
 
     private void throwError(Errors error) {
